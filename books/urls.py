08d04fcedd8840a7f6f93c6dd07242f82f0b5603
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-12-02 20:59:22 +0600

    viewsets & routers

--- books/urls.py
+++ books/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from books.views import BookListAPIView, BookDetailView, BookCreateView, BookUpdateView, AuthorListView
+from books.views import BookListAPIView, BookDetailView, BookCreateView, BookUpdateView, AuthorListView, BookViewSet, AuthorViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('', BookListAPIView.as_view(), name='book_list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('authors/', AuthorListView.as_view(), name='authors'),
+router = DefaultRouter()
+router.register('books', BookViewSet, basename='book')
+router.register('authors', AuthorViewSet, basename='author')
 
-    # path('books/<int:pk>/delete/', BookDetailView.as_view(), name='book-delete'),
+urlpatterns = router.urls
 
-]
 
+
+class Dog:
+    pass
+
+dog1 = Dog()
--- books/views.py
+++ books/views.py
@@ -9,35 +9,40 @@ from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer, BookCreateSerializer
 
 
-class BookListAPIView(ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
-class BookDetailView(RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
-class BookCreateView(CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookCreateSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user.username)
-
-
-class BookUpdateView(UpdateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookCreateSerializer
-
-    def perform_update(self, serializer):
-        serializer.save(updated_at=timezone.now())
-
-
-class BookDeleteView(DestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class BookListAPIView(ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#
+#
+# class BookDetailView(RetrieveAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#
+#
+# class BookCreateView(CreateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookCreateSerializer
+#
+#     def perform_create(self, serializer):
+#         serializer.save(author=self.request.user.username)
+#
+#
+# class BookUpdateView(UpdateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookCreateSerializer
+#
+#     def perform_update(self, serializer):
+#         serializer.save(updated_at=timezone.now())
+#
+#
+# class BookDeleteView(DestroyAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#
+#
+# class AuthorListView(ListCreateAPIView):
+#     queryset = Author.objects.all()
+#     serializer_class = AuthorSerializer
 
 
 class BookViewSet(ModelViewSet):
@@ -45,7 +50,7 @@ class BookViewSet(ModelViewSet):
     serializer_class = BookSerializer
 
 
-class AuthorListView(ListCreateAPIView):
+class AuthorViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
