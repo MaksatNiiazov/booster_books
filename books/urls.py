cd691e6bac09d788328a3cf266609cf5ea5176f9
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-12-02 21:29:56 +0600

    viewsets & routers

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path
 from books.views import BookViewSet, AuthorViewSet # BookListAPIView, BookDetailView, BookCreateView, BookUpdateView, AuthorListView,
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('books', BookViewSet, basename='book')
-router.register('authors', AuthorViewSet, basename='author')
+router.register('books/', BookViewSet, basename='book')
+router.register('authors/', AuthorViewSet, basename='author')
 
 urlpatterns = router.urls
